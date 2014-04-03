30010a1181a260742cc0e468caff4486c2189bd1
Christos Louizos <chr.louizos@gmail.com>
2014-04-03 15:57:03 +0200

    Updated IBM1 model

--- IBM1.py
+++ IBM1.py
@@ -1,3 +1,4 @@
+from __future__ import division
 from collections import defaultdict
 
 
@@ -10,8 +11,55 @@ class p_sentence(object):
 
 class IBM1(object):
 
-    def __init__(self, p_sentences):
+    def __init__(self, p_sentences, converge_thres):
         self.p_sentences = p_sentences
+        self.converge_thres = converge_thres
+        self.probabilities = None
+        self._generate_voc()
+
+    def _generate_voc(self):
+        self.voc_e = set()
+        self.voc_f = set()
+
+        for sent in p_sentences:
+            self.voc_e.update(sent.words_e)
+            self.voc_f.update(sent.words_f)
+
+        #self.voc_f.add(None)
+
+    def train(self):
+        t = defaultdict(lambda: 1.0/len(self.voc_f))
+        converged = False
+        iteration = 0
+
+        while not converged:
+            print 'EM iteration %i' % iteration
+            # init count(e|f) and total(f)
+            count = defaultdict(float)
+            total = defaultdict(float)
+
+            for sent in self.p_sentences:
+                for e in sent.words_e:
+                    total_s = 0
+                    for f in sent.words_f:
+                        total_s += t[e, f]
+                for e in sent.words_e:
+                    for f in sent.words_f:
+                        count[e, f] += t[e, f]/total_s
+                        total[f] += t[e, f]/total_s
+                        print total[f], count[e, f], t[e, f], total_s
+
+            for f in self.voc_f:
+                for e in self.voc_e:
+                    print f, e
+                    t[e, f] = count[e, f] / total[f]
+
+            iteration += 1
+            for key, value in t.iteritems():
+                print key, value
+
+            if iteration == 15:
+                break
 
 
 if __name__ == '__main__':
@@ -23,3 +71,6 @@ if __name__ == '__main__':
 
     for p_sent in p_sentences:
         print p_sent.words_e, p_sent.words_f
+
+    ibm1 = IBM1(p_sentences, 1e-2)
+    ibm1.train()
