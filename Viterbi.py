48ab83961cb3fffd3cd5e43cbe924804849cbfdf
saidalfaraby <said.al.faraby@gmail.com>
2014-04-04 11:42:11 +0200

    update viterbi, add simple max

--- Viterbi.py
+++ Viterbi.py
@@ -10,14 +10,14 @@ def viterbi(ibm1, sent):
     # Initialize base cases (t == 0)
     for f in sent.words_f:
         V[0][f] = 1.0
-        path[f] = [f]
+        path[f] = []
 
-    for ie in range(1, len(sent.words_e)):
+    for ie in range(1, len(sent.words_e)+1):
         V.append({})
         newpath = {}
 
         for f in sent.words_f:
-            (prob, state) = max((V[ie-1][y0] * t[sent.words_e[ie], f], y0) for y0 in sent.words_f)
+            (prob, state) = max((V[ie-1][y0] * t[sent.words_e[ie-1], f], y0) for y0 in sent.words_f)
             V[ie][f] = prob
             newpath[f] = path[state] + [f]
 
@@ -27,6 +27,16 @@ def viterbi(ibm1, sent):
     #print_dptable(V)
     (prob, state) = max((V[ie][y], y) for y in sent.words_f)
     return (prob, path[state])
+
+def simpleMax(ibm1, sent):
+    t = ibm1.probabilities
+    prob = 1.0
+    path = []
+    for e in sent.words_e:
+        newprob, state = max((t[e, f], f) for f in sent.words_f)
+        prob *= newprob
+        path.append(state)
+    return (prob, path)
 
 if __name__ == '__main__':
 
@@ -48,7 +58,16 @@ if __name__ == '__main__':
     for elem in p_corp:
         sentences.append(Pair_sent(elem))
 
+    print 'viterbi'
     for sent in sentences:
     	prob, path = viterbi(ibm1, sent)
     	print sent.words_e, sent.words_f
         print path
+        print prob
+
+    print 'simple Max'
+    for sent in sentences:
+        prob, path = simpleMax(ibm1, sent)
+        print sent.words_e, sent.words_f
+        print path
+        print prob
