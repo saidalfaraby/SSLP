ac86ddb3e0cf18394a6e0a99326060037cc8877d
Christos Louizos <chr.louizos@gmail.com>
2014-04-21 13:07:12 +0200

    Removed old version and renamed the new one.

--- Project2/extract_phrases.py
+++ Project2/extract_phrases.py
@@ -32,79 +32,85 @@ def parse_aligned_sent(path_en, path_nl, path_al, how_many):
     return aligned_sent
 
 
-def parse_phrases(aligned_sent):
+def parse_phrases(aligned_sent, max_len=4):
     phrase_pairs = set()
+    # how to estimate the joint probability?
+    # number of occurences of the phrase, divided by the number of sentences?
+    # probably divided by the number of phrases
+    joint_ennl = defaultdict(float)
+    nl_given_en = defaultdict(lambda: defaultdict(float))
+    en_given_nl = defaultdict(lambda: defaultdict(float))
     for sent in aligned_sent:
+        aligned = defaultdict(lambda: False)
         for e_start in xrange(len(sent.w_nl)):
-            for e_end in xrange(e_start, len(sent.w_nl)):
-                f_start, f_end = len(sent.w_en), 0
+            for e_end in xrange(e_start, e_start + max_len):
+                f_start, f_end = len(sent.w_en), -1
                 for e, f in sent.al:
+                    aligned[f] = True
                     if e_start <= e and e <= e_end:
                         f_start = min(f, f_start)
                         f_end = max(f, f_end)
                 # print 'f_start', f_start, 'f_end', f_end, 'e_start', e_start, 'e_end', e_end
-                E = extract(f_start, f_end, e_start, e_end, sent.al, sent.w_en, sent.w_nl)
+                # E = extract(f_start, f_end, e_start, e_end, sent.al, sent.w_en, sent.w_nl)
+                E = extract(f_start, f_end, e_start, e_end, sent.al, sent.w_en, sent.w_nl, aligned)
+                for elem in E:
+                    joint_ennl[elem] += 1
+                    nl_given_en[elem[1]][elem[0]] += 1
+                    en_given_nl[elem[0]][elem[1]] += 1
+
                 phrase_pairs.update(E)
 
-    for elem in phrase_pairs:
-        pair = elem.split('#')
-        for i in pair:
-            print i
-        print
-    return phrase_pairs
+    # normalize the conditionals
+    for key in en_given_nl:
+        denom_en = sum([en_given_nl[key][key2] for key2 in en_given_nl[key]])
+        for key2 in en_given_nl[key]:
+            en_given_nl[key][key2] /= denom_en
+
+    for key in nl_given_en:
+        denom_nl = sum([nl_given_en[key][key2] for key2 in nl_given_en[key]])
+        for key2 in nl_given_en[key]:
+            nl_given_en[key][key2] /= denom_nl
+
+    # normalize the joint
+    for key in joint_ennl:
+        joint_ennl[key] /= len(phrase_pairs)
+
+    return phrase_pairs, en_given_nl, nl_given_en, joint_ennl
 
 
-def extract(f_start, f_end, e_start, e_end, w_a, w_en, w_nl):
-    aligned_fe = defaultdict(lambda: False)
-    aligned_fs = defaultdict(lambda: False)
+# def extract(f_start, f_end, e_start, e_end, w_a, w_en, w_nl):
+def extract(f_start, f_end, e_start, e_end, w_a, w_en, w_nl, aligned):
+    # print f_start, f_end, e_start, e_end
+    # aligned_fe = defaultdict(lambda: False)
+    # aligned_fs = defaultdict(lambda: False)
     # print
     # print 'f_start', f_start, 'f_end', f_end, 'e_start', e_start, 'e_end', e_end
-    if f_end == 0:
+    if f_end == -1:
         # print 'zero f_end'
         return set()
     for e, f in w_a:
-        if e < e_start or e > e_end:
-            # print 'violating consistency'
-            return set()
+        if f >= f_start and f <= f_end:
+            if e < e_start or e > e_end:
+                # print 'violating consistency'
+                return set()
     E = set()
     f_s = f_start
     # print 'passed and adding possible phrases'
     while True:
-        # flag1 = 0
         f_e = f_end
         while True:
-            # flag = 0
-            if abs(e_start - e_end) <= 4:
-                E.add(' '.join(w_nl[e_start:e_end]) + ' # ' + ' '.join(w_en[f_s:f_e]))
-            aligned_fe[f_e] = True
+            #if abs(e_start - e_end) <= 3:
+            E.add((' '.join(w_nl[e_start:e_end+1]), ' '.join(w_en[f_s:f_e+1])))
+            # aligned_fe[f_e] = True
             f_e += 1
-            if aligned_fe[f_e] or f_e == len(w_en):
+            # if aligned_fe[f_e] or f_e == len(w_en):
+            if aligned[f_e] or f_e == len(w_en):
                 break
-            # for element in E:
-            #     try:
-            #         if w_nl[f_e] in element:
-            #             flag = 1
-            #             break
-            #     except:
-            #         flag = 1
-            #         break
-            # if flag == 1:
-            #     break
-            # print f_e
-        aligned_fs[f_s] = True
+        # aligned_fs[f_s] = True
         f_s -= 1
-        if aligned_fs[f_s] or f_s < 0:
+        # if aligned_fs[f_s] or f_s < 0:
+        if aligned[f_s] or f_s < 0:
             break
-        # for element in E:
-        #     try:
-        #         if w_nl[f_s] in element:
-        #             flag1 = 1
-        #             break
-        #     except:
-        #         flag1 = 1
-        #         break
-        # if flag1 == 1:
-        #     break
 
     return E
 
@@ -114,6 +120,23 @@ if __name__ == '__main__':
     en_corp = 'p2_training.en'
     nl_corp = 'p2_training.nl'
     al_corp = 'p2_training_symal.nlen'
-    how_many = 1000
+    how_many = 1
+    max_len = 4
     aligned_sent = parse_aligned_sent(folder+en_corp, folder+nl_corp, folder+al_corp, how_many)
-    parse_phrases(aligned_sent)
+    phrase_pairs, en_given_nl, nl_given_en, joint_ennl = parse_phrases(aligned_sent, max_len=max_len)
+
+    print 'P(en|nl)'
+    for key in en_given_nl:
+        for key2 in en_given_nl[key]:
+            print key, '#', key2, ':', en_given_nl[key][key2]
+
+    print
+    print 'P(nl|en)'
+    for key in nl_given_en:
+        for key2 in nl_given_en[key]:
+            print key, '#', key2, ':', nl_given_en[key][key2]
+
+    print
+    print 'P(en, nl)'
+    for key in joint_ennl:
+        print key, ':', joint_ennl[key]
