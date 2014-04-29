61b658500c209a857e81b9525e07649acc05975d
Christos Louizos <chr.louizos@gmail.com>
2014-04-29 13:51:20 +0200

    PEP8

--- Project2/construct_ITG_phrases.py
+++ Project2/construct_ITG_phrases.py
@@ -1,51 +1,55 @@
 from collections import defaultdict
 
+
 class Node (object):
-	"""docstring for Tree """
-	def __init__(self, label=None):
+    """docstring for Tree """
+    def __init__(self, label=None):
 		# super(Tree , self).__init__()
 		self.label = label
 		self.left = None
 		self.right = None
 
+
 # string = ''
 def printTree(T):
 	def recur(T):
-		if T.label!=None :
+		if T.label is not None:
 			return str(T.label)+' '
-		else :
+		else:
 			string = ''
-			string+=recur(T.left)
-			string+=recur(T.right)
+			string += recur(T.left)
+			string += recur(T.right)
 			return string
 	return recur(T)
 
-def getITGPermut(lb,ub):
+
+def getITGPermut(lb, ub):
 	'''
 	lb : lower bound of permuting integer
 	ub : upper bound of permuting integer
 	return set of itg-constrained string
 	'''
 	def recur(start, end):
-		if end - start <=1:
+		if end - start <= 1:
 			return [Node(start)]
-		else :
+		else:
 			all_t = []
-			for k in xrange(start+1,end):
-				for i in recur(start,k):
-					for j in recur(k,end):
-						for l,r in [(i,j),(j,i)]:
+			for k in xrange(start+1, end):
+				for i in recur(start, k):
+					for j in recur(k, end):
+						for l, r in [(i, j), (j, i)]:
 							t = Node()
 							t.left = l
-							t.right= r
+							t.right = r
 							all_t.append(t)
 			return all_t
 
 	allString = set()
-	allItg = recur(lb,ub)
+	allItg = recur(lb, ub)
 	for t in allItg:
 		allString.add(printTree(t))
 	return allString
+
 
 def permut_phrases(phrase_pairs, itg_permut):
 	'''
@@ -63,8 +67,9 @@ def permut_phrases(phrase_pairs, itg_permut):
 		for i in range(len(itglist)):
 			left.append(phrase_pairs[i][0])
 			right[itglist[i]] = phrase_pairs[i][1]
-		all_permuted.append((' '.join(left),' '.joint(right)))
+		all_permuted.append((' '.join(left), ' '.joint(right)))
 	return all_permuted
+
 
 def devide_phrases_accto_length(all_phrase_pairs):
 	'''
@@ -73,28 +78,29 @@ def devide_phrases_accto_length(all_phrase_pairs):
 	'''
 	div_phrases = defaultdict(list)
 	for p in all_phrase_pairs:
-		div_phrases[max(len(p[0].split()),len(p[1].split()))].append(p)
+		div_phrases[max(len(p[0].split()), len(p[1].split()))].append(p)
 	return div_phrases
+
 
 def construct_itg_phrases(all_phrase_pairs):
 	div_phrases = devide_phrases_accto_length(all_phrase_pairs)
 	max_length = 4
 	#find all combination of length that sum to 2 up to max_length
 	length_combination = []
-	for l in range(2,max_length+1):
-		for m in range(2,l-1):
+	for l in range(2, max_length+1):
+		for m in range(2, l-1):
 			pass
 
 
 def test():
-	def findAllSumTo(N, choose):
-		if N==0:
+	def findAllSumTo(N):
+		if N == 0:
 			return [[]]
-		else :
+		else:
 			choose = []
-			for j in range(1,N+1):
+			for j in range(1, N+1):
 				# choose.append(j)
-				N-=j
+				N -= j
 				for i in findAllSumTo(N):
 					print i
 					choose.append(i.append(j))
@@ -103,8 +109,7 @@ def test():
 	t = findAllSumTo(4)
 	print len(t)
 	for i in t:
-		print i 
-
+		print i
 
 
 if __name__ == '__main__':
@@ -113,6 +118,3 @@ if __name__ == '__main__':
 	# for s in allString:
 	# 	print s
 	test()
-
-
-		
