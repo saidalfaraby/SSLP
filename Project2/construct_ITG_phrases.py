5bc15feea028e2891ad7852e83b89bfc31e35acd
saidalfaraby <said.al.faraby@gmail.com>
2014-04-29 13:41:29 +0200

    construct length combination

--- Project2/construct_ITG_phrases.py
+++ Project2/construct_ITG_phrases.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 class Node (object):
 	"""docstring for Tree """
 	def __init__(self, label=None):
@@ -20,8 +22,8 @@ def printTree(T):
 
 def getITGPermut(lb,ub):
 	'''
-	params lb-lower bound
-	params ub-upper bound
+	lb : lower bound of permuting integer
+	ub : upper bound of permuting integer
 	return set of itg-constrained string
 	'''
 	def recur(start, end):
@@ -65,22 +67,52 @@ def permut_phrases(phrase_pairs, itg_permut):
 	return all_permuted
 
 def devide_phrases_accto_length(all_phrase_pairs):
-	#dictionary with length as key and list of phrase_pairs as value
+	'''
+	return dictionary with length as key and list of phrase_pairs as value
+	we will take max length between english and foreign phrase
+	'''
+	div_phrases = defaultdict(list)
 	for p in all_phrase_pairs:
-		
+		div_phrases[max(len(p[0].split()),len(p[1].split()))].append(p)
+	return div_phrases
+
+def construct_itg_phrases(all_phrase_pairs):
+	div_phrases = devide_phrases_accto_length(all_phrase_pairs)
+	max_length = 4
+	#find all combination of length that sum to 2 up to max_length
+	length_combination = []
+	for l in range(2,max_length+1):
+		for m in range(2,l-1):
+			pass
 
 
+def test():
+	def findAllSumTo(N, choose):
+		if N==0:
+			return [[]]
+		else :
+			choose = []
+			for j in range(1,N+1):
+				# choose.append(j)
+				N-=j
+				for i in findAllSumTo(N):
+					print i
+					choose.append(i.append(j))
+			return choose
 
-
-
+	t = findAllSumTo(4)
+	print len(t)
+	for i in t:
+		print i
 
 
 
 if __name__ == '__main__':
-	allString = getITGPermut(1,5)
-	print len(allString)
-	for s in allString:
-		print s
+	# allString = getITGPermut(1,5)
+	# print len(allString)
+	# for s in allString:
+	# 	print s
+	test()
 
 
 		
