837edcfe411aa845e4a0416d404177207a7090da
saidalfaraby <said.al.faraby@gmail.com>
2014-04-30 11:54:04 +0200

    update itg phrases

--- Project2/construct_ITG_phrases.py
+++ Project2/construct_ITG_phrases.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-from itertools import permutations
+from extract_phrases import load_phrases
 
 
 class Node (object):
@@ -10,32 +10,25 @@ class Node (object):
 		self.left = None
 		self.right = None
 
-
-class Combo(object):
-	def __init__(self):
-		self.combos = set()
-
-	def update_combos(self, new_combos):
-		self.combos.update({' '.join(map(str, i)) for i in new_combos})
-
-	def generate_all_combos(self):
-		self.all = set()
-		y = [map(int, i.split()) for i in self.combos]
-		for elem in y:
-			self.all.update(permutations(elem))
+class Array(object):
+	"""docstring for Array"""
+	def __init__(self, label=None):
+		self.label = label
+		self.children = []
+		self.parent = None
+		
 
 
-# string = ''
 def printTree(T):
-	def recur(T):
-		if T.label is not None:
-			return str(T.label)+' '
-		else:
-			string = ''
-			string += recur(T.left)
-			string += recur(T.right)
-			return string
-	return recur(T)
+	# def recur(T):
+	if T.label is not None:
+		return str(T.label)+' '
+	else:
+		string = ''
+		string += printTree(T.left)
+		string += printTree(T.right)
+		return string
+	# return recur(T)
 
 
 def getITGPermut(lb, ub):
@@ -86,14 +79,35 @@ def permut_phrases(phrase_pairs, itg_permut):
 	return all_permuted
 
 
-def devide_phrases_accto_length(all_phrase_pairs):
+def devide_phrases_accto_length(all_phrase_pairs, joint_phrase_table):
 	'''
 	return dictionary with length as key and list of phrase_pairs as value
 	we will take max length between english and foreign phrase
 	'''
 	div_phrases = defaultdict(list)
+
+	max_prob_per_length = defaultdict(lambda : 0)
+	sum_prob_per_length = defaultdict(list)
+	for pp in joint_phrase_table:
+		l = max(len(pp[0].split()), len(pp[1].split()))
+		# if joint_phrase_table[pp]>= max_prob_per_length[l]:
+			# max_prob_per_length[l] = joint_phrase_table[pp]
+		sum_prob_per_length[l].append(joint_phrase_table[pp])
+	for k in sum_prob_per_length:
+		max_prob_per_length[k] = sum(sum_prob_per_length[k])/len(sum_prob_per_length[k])
+
+
+	print 'max prob per length', max_prob_per_length
+
+
 	for p in all_phrase_pairs:
-		div_phrases[max(len(p[0].split()), len(p[1].split()))].append(p)
+		pl = max(len(p[0].split()), len(p[1].split()))
+		if joint_phrase_table[p]>= max_prob_per_length[pl]:
+			div_phrases[pl].append(p)
+	le = [len(div_phrases[l]) for l in div_phrases]
+	print 'for all length ',le
+	print 'sum of all length ', sum(le)
+	print 'original length ', len(all_phrase_pairs)
 	return div_phrases
 
 
@@ -101,56 +115,68 @@ def construct_itg_phrases(all_phrase_pairs):
 	div_phrases = devide_phrases_accto_length(all_phrase_pairs)
 	max_length = 4
 	#find all combination of length that sum to 2 up to max_length
-	length_combination = []
+	length_combinations = []
 	for l in range(2, max_length+1):
-		for m in range(2, l-1):
+		length_combinations += getAllLengthComb(l)
+	#devide length_combination into number of part ex : from 2 parts up to 4 parts
+	#save into dictionary with number of parts as the key
+	length_dict = defaultdict(list)
+	for lc in length_combinations:
+		length_dict[len(lc)].append(lc)
+
+	#there is a more efficient way by using smaller part, but going for correctness first
+	for l in length_dict.keys():
+		itgPermut = getITGPermut(0,l)
+		for c in length_dict[l]:
 			pass
 
 
-def find_combos(N):
-	def subset_sum(numbers, target, cmb, partial=[], so_far=[]):
-		s = sum(partial)
-		# check if the partial sum is equals to target
-		if s == target:
-			so_far.append(partial)
-			cmb.update_combos(so_far)
-			# print "sum(%s)=%s" % (partial, target)
-		if s >= target:
-			return  # if we reach the number why bother to continue
-		for i in range(len(numbers)):
-			n = numbers[i]
-			remaining = numbers[i+1:]
-			subset_sum(remaining, target, cmb, partial + [n], so_far)
-		#print [map(' '.join, i) for i in so_far]
-
-	cmb = Combo()
-	elements = []
-	for i in xrange(1, N):
-		for k in xrange(int(N/i)):
-			elements.append(i)
-	subset_sum(elements, N, cmb)
-	cmb.generate_all_combos()
-	return cmb
-
-
-def test():
+def getAllLengthComb(maxLength):
+# def all_length_comb(Length):
 	def findAllSumTo(N):
-		if N == 0:
-			return [[]]
-		else:
-			choose = []
-			for j in range(1, N+1):
-				choose.append(j)
-				N -= j
-				for i in findAllSumTo(N):
-					print i
-					#choose.append(i.append(j))
-			return choose
+		final = []
+		ind = range(1,N+1)
+		for j in ind:
+			choose = Array(j)
+			Next = N-j
+			if N > 0:
+				ss =findAllSumTo(Next)
+				for i in ss: 
+					choose.children.append(i)
+					i.parent = choose
+			final.append(choose)
+		return final
+		# return findAllSumTo(Length)
 
-	t = findAllSumTo(4)
-	print len(t)
-	for i in t:
-		print i
+	tree = findAllSumTo(maxLength)
+
+	def getAllPath(T):
+		allLeaves = []
+		#recur All leaves
+		def recur(T):
+			if len(T.children)==0:
+				allLeaves.append(T)
+			else :
+				for i in (T.children):
+					recur(i)
+		recur(T)
+
+		#for each leaves, travel to root until parent = None
+		allcomb = []
+		for leave in allLeaves:
+			l = leave
+			comb = [l.label]
+			while l.parent!=None:
+				l = l.parent
+				comb.append(l.label)
+			if len(comb)>1:
+				allcomb.append(comb)
+		return allcomb
+
+	allComb = []
+	for t in tree :
+		allComb += getAllPath(t)
+	return allComb
 
 
 if __name__ == '__main__':
@@ -158,6 +184,19 @@ if __name__ == '__main__':
 	# print len(allString)
 	# for s in allString:
 	# 	print s
-	cmb = find_combos(4)
-	print cmb.all
-	# print cmb.combos
+	# t = findAllSumTo(4)
+	# print len(t)
+	# test = []
+	# for i in t:
+	# 	test+=printTest(i)
+	# print test
+
+	phrase_pairs, en_given_nl, nl_given_en, joint_ennl = load_phrases('training/')
+
+	devide_phrases_accto_length(phrase_pairs, joint_ennl)
+
+	t =getAllLengthComb(4)
+	print t
+
+	t = getITGPermut(0,4)
+	print t
