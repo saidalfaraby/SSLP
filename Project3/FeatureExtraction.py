cc71c52ac2ac926b28bee03f5b53a5a9e5d0aef9
saidalfaraby <said.al.faraby@gmail.com>
2014-05-20 12:03:26 +0200

    update

--- Project3/FeatureExtraction.py
+++ Project3/FeatureExtraction.py
@@ -7,39 +7,42 @@ import string
 from nltk.corpus import stopwords
 import dill as pickle
 
-
 class Features(object):
   """docstring for Features"""
   def __init__(self):
-    self.Term_Freq = defaultdict(lambda: 0)  # term frequency. Key = term, Val = frequency
-    self.POS_Freq = defaultdict(lambda: 0)  # postag frequency. Key = (term,POSTag), Val = Frequency
-    self.Trans_Freq = defaultdict(lambda: 0)  # translation frequency. Key = (E,F), Val = frequency
+    self.Term_Freq = defaultdict(lambda:0) #term frequency. Key = term, Val = frequency
+    self.POS_Freq = defaultdict(lambda:0) #postag frequency. Key = (term,POSTag), Val = Frequency
+    self.Trans_Freq = defaultdict(lambda:0) #translation frequency. Key = (E,F), Val = frequency
     self.N_Term = 0
 
   def parse_doc(self, path):
     regex = re.compile('[%s]' % re.escape(string.punctuation))
     f = open(path)
-    i = 0
+    i=0
     for sentence in f.readlines():
       sentence = nltk.pos_tag(nltk.word_tokenize(sentence))
-      i += 1
+      i+=1
       print i
       #remove punctuation
       new_s = []
-      for token, pos in sentence:
-        try:
+      for token,pos in sentence: 
+        try :
           new_token = regex.sub(u'', token).decode('utf-8')
           if not new_token == u'' and not new_token in stopwords.words('english'):
-            self.update_count(new_token, pos)
-        except:
+            self.update_count(new_token,pos)
+        except :
           pass
-    self.Term_Freq.default_factory = lambda: 0.1
-    self.POS_Freq.default_factory = lambda: 0.1
-    self.Trans_Freq.default_factory = lambda: 0.1
+    self.set_lambda(0.1)
 
-  def update_count(self, t, p):
-    self.Term_Freq[t] += 1
-    self.POS_Freq[(t, p)] += 1
+  def set_lambda(self,v):
+    self.Term_Freq.default_factory = lambda: v
+    self.POS_Freq.default_factory = lambda: v
+    self.Trans_Freq.default_factory = lambda: v
+          
+  
+  def update_count(self,t,p):
+    self.Term_Freq[t]+=1
+    self.POS_Freq[(t,p)]+=1
     self.N_Term += 1
 
   def save(self, path='features_data.pickle'):
@@ -61,12 +64,10 @@ class Features(object):
     self.POS_Freq = storage['POS_Freq']
     self.Trans_Freq = storage['Trans_Freq']
     self.N_Term = storage['N_Term']
-    self.Term_Freq.default_factory = lambda: 0.1
-    self.POS_Freq.default_factory = lambda: 0.1
-    self.Trans_Freq.default_factory = lambda: 0.1
+    self.set_lambda(0.1)
     self.log('loaded..')
 
-  def log(self, string):
+  def log(self,string):
     f = open('log_FE.txt', 'a+')
     print string
     f.write(string+'\n')
@@ -82,3 +83,7 @@ if __name__ == '__main__':
     print F.N_Term
     # print F.Term_Freq.keys()[0]#, F.Term_Freq(F.Term_Freq.keys()[0])
     # print F.POS_Freq[('even','RB')]#, F.POS_Freq(F.POS_Freq.keys()[0])
+
+
+
+
--- Project3/IntelligentSelection.py
+++ Project3/IntelligentSelection.py
@@ -6,85 +6,97 @@ import nltk
 from nltk.corpus import stopwords
 from FeatureExtraction import Features
 import re
+import random
 import string
-
 
 class IntelligentSelection(object):
   """docstring for IntelligentSelection"""
-  def __init__(self, Features):
-    self.Features = Features
-    self.MixDoc = []  # store object of docID and pos tagged sentences, and score
-    self.selectedDoc = []
+  def __init__(self, In_Model, Mix_Model=None):
+    self.In_Model = In_Model
+    self.Mix_Model = Mix_Model
+    self.Mix_Docs = [] #store object of docID and pos tagged sentences, and score
+    self.Selected_Docs = []
     self.scores = []
+    self.dual_score = True
+    self.include_pos = False
 
-  def entropy_score(self, termpos1, termpos2=None):
+
+  def entropy_score(self,termpos, find_threshold=False):
   #score based on difference of cross entropy
   #sentences have been tokenized and pos tagged
-
-    F = self.Features
-    score1 = 0
-    score2 = 0
-    for t, p in termpos1:
-      score1 += -np.log(F.Term_Freq[t]/F.N_Term) + -np.log(F.POS_Freq[t, p]/F.N_Term)
-    try:
-      score1 /= len(termpos1)
-    except:
-      score1 = 99999
-    if termpos2 is not None:
-      for t, p in termpos2:
-        score2 += -np.log(F.Term_Freq[t]/F.N_Term) + -np.log(F.POS_Freq[t, p]/F.N_Term)
-      try:
-        score2 /= len(termpos2)
-      except:
-        score2 = 99999
-    return score1 - score2
-
+    
+    IN = self.In_Model
+    MIX = self.Mix_Model
+    sc_term_in = 0
+    sc_term_mix = 0
+    sc_pos_in = 0
+    sc_pos_mix = 0
+    if len(termpos)==0 or MIX.N_Term<-99999:
+      return -99999 if find_threshold else 99999
+    for t,p in termpos:
+      sc_term_in += -np.log(IN.Term_Freq[t]/IN.N_Term) 
+      sc_pos_in+= -np.log(IN.POS_Freq[t,p]/IN.N_Term)
+    if MIX!=None and self.dual_score:
+      sc_term_mix += -np.log(MIX.Term_Freq[t]/MIX.N_Term) 
+      sc_pos_mix+= -np.log(MIX.POS_Freq[t,p]/MIX.N_Term)
+    if self.include_pos :
+      return ((sc_term_in+sc_pos_in)-(sc_term_mix+sc_pos_mix))/len(termpos)
+    return (sc_term_in-sc_term_mix)/len(termpos)
   def select(self, threshold, is_update=False):
     self.log('selecting ...')
-    for mix in self.MixDoc:
+    for mix in self.Mix_Docs:
       mix['score'] = self.entropy_score(mix['termpos'])
-      if mix['score'] < 99999:
+      if mix['score']<99999:
         self.scores.append(mix['score'])
-      if mix['score'] < threshold:
-        self.selectedDoc.append(mix['docID'])
-
+      if mix['score'] > threshold:
+        self.Selected_Docs.append(mix['docID'])
+        
     self.log('finish selecting')
 
-  def measure(self, label):
-    print 'label', len(label)
-    print 'retrieve', len(self.selectedDoc)
-    intersection = list(set(label) & set(self.selectedDoc))
-    print 'inter', len(intersection)
-    return {'precision': len(intersection)/len(self.selectedDoc), 'recall': len(intersection)/len(label)}
+  def measure(self,label):
+    self.log('in domain : '+str(len(label)))
+    self.log('retrieve :'+str(len(self.Selected_Docs)))
+    intersection = list(set(label)&set(self.Selected_Docs))
+    self.log('intersection : '+str(len(intersection)))
+    return {'precision': len(intersection)/len(self.Selected_Docs), 'recall':len(intersection)/len(label)}
 
   def findThreshold(self):
     self.log('finding threshold')
     maxScore = -99999999
-    for mix in self.MixDoc:
-      score = self.entropy_score(mix['termpos'])
-      if score > maxScore:
+    allScore = []
+    for mix in self.Mix_Docs:
+      score = self.entropy_score(mix['termpos'],True)
+      if score > -99999:
+        allScore.append(score)
+      if score>maxScore:
         maxScore = score
+    self.log('threshold statistics')
     self.log('threshold = '+str(maxScore))
+    allScore = np.asarray(allScore)
+    self.log('max score '+str(np.max(allScore)))
+    self.log('min score '+str(np.min(allScore)))
+    self.log('avg score '+str(np.mean(allScore)))
+    self.log('std score '+str(np.std(allScore)))
     return maxScore
 
-  def parse_mix_doc(self, path):
+  def parse_mix_doc(self,path):
     regex = re.compile('[%s]' % re.escape(string.punctuation))
     self.log('parsing documents from '+path)
-    self.MixDoc = []
+    self.Mix_Docs = []
     f = open(path)
     docId = 0
     for sentence in f.readlines():
       sentence = nltk.pos_tag(nltk.word_tokenize(sentence))
       s = []
-      for token, pos in sentence:
-        try:
+      for token,pos in sentence: 
+        try :
           new_token = regex.sub(u'', token).decode('utf-8')
           if not new_token == u'' and not new_token in stopwords.words('english'):
-            s.append((new_token, pos))
-        except:
+            s.append((new_token,pos))
+        except :
           pass
-      self.MixDoc.append({'docID': docId, 'termpos': s, 'score': -999999})
-      docId += 1
+      self.Mix_Docs.append({'docID':docId, 'termpos':s, 'score':-999999})
+      docId +=1
     self.log('parsed..')
 
   def stats(self):
@@ -98,40 +110,67 @@ class IntelligentSelection(object):
   def save(self, path='mix_doc.pickle'):
     self.log('save data to '+path)
     with open(path, 'wb') as handle:
-      pickle.dump(self.MixDoc, handle)
+      pickle.dump(self.Mix_Docs, handle)
     self.log('saved..')
 
   def load(self, path='mix_doc.pickle'):
     self.log('loading data '+path)
     with open(path, 'rb') as handle:
-      self.MixDoc = pickle.load(handle)
+      self.Mix_Docs = pickle.load(handle)
     self.log('loaded..')
+    self.log('number of docs loaded : '+str(len(self.Mix_Docs)))
 
-  def log(self, string):
+  def log(self,string):
     f = open('log_IS.txt', 'a+')
     print string
     f.write(string+'\n')
     f.close()
 
 if __name__ == '__main__':
-  th = 17.3
+  th = 10
 
-  Model = Features()
-  Model.load()
-  IS = IntelligentSelection(Model)
+  In_Model = Features()
+  Mix_Model = Features()
+  In_Model.load()
+  IS = IntelligentSelection(In_Model,Mix_Model)
+  IS.include_pos = False
+  IS.dual_score = True
   IS.log('---------------------------')
   IS.log('run with threshold = '+str(th))
   # IS.parse_mix_doc('project3_data_selection/legal.dev.en')
   # th = IS.findThreshold()
   # IS.parse_mix_doc('project3_data_selection/out.mixed.legal.en')
-  # IS.save()
-  # IS.load()
+  # IS.save('legal_dev_doc.pickle')
+  IS.load()
+  
+  for idx in random.sample(range(500000),50000):
+    for t,p in IS.Mix_Docs[idx]['termpos']:
+      Mix_Model.update_count(t,p)
+  print Mix_Model.N_Term
+  print len(Mix_Model.Term_Freq)
+  IS.Mix_Model = Mix_Model
+  # Mix_Model.save('mix_model.pickle')
+  # Mix_Model.load('mix_model.pickle')
+  
+
+  # IS.parse_mix_doc('project3_data_selection/legal.dev.en')
+  # th = IS.findThreshold()
+  # IS.parse_mix_doc('project3_data_selection/out.mixed.legal.en')
+  # IS.save('legal_dev_doc.pickle')
+
+  # th=IS.findThreshold()
   IS.select(th)
   # label = range(2000)
-  label = range(450000, 500000)
+  label = range(450000,500000)
   m = IS.measure(label)
   IS.log('precision '+str(m['precision']))
   IS.log('recall '+str(m['recall']))
 
   IS.stats()
   IS.log('\n\n')
+
+
+
+
+
+    
