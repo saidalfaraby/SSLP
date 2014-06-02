9a96261b81bdb1d95530914dc3e96ee136a2b5cb
Christos Louizos <chr.louizos@gmail.com>
2014-06-02 10:11:37 +0200

    Updated experiments. Added some results

--- Project3/Experiments.py
+++ Project3/Experiments.py
@@ -89,7 +89,7 @@ def experiment_w2vKLIEP(type_d, type_pooling='max_p'):
     kliep.fit_CV(test_data, train_data)
     predictions = - np.ones(len(uni_sentences_out))
     w = kliep.predict(test_data).ravel()
-    predictions[np.where(w > 1.5)[0]] = 1   # w = p_te/p_tr
+    predictions[np.where(w > 2.)[0]] = 1   # w = p_te/p_tr
     print 'total positive:', np.where(predictions == 1)[0].shape, ', out of:', test_data.shape[0]
     # sorted_ind = np.argsort(w, axis=None)[::-1]
     # predictions[sorted_ind[0:50000]] = 1
@@ -102,7 +102,7 @@ def experiment_w2vKLIEP(type_d, type_pooling='max_p'):
     print 'Support:', s,
 
 
-def experiment_tfidfSVM(type_d, topics=100):
+def experiment_tfidfSVM(type_d, topics=100, method='tsvd'):
     if type_d is 'legal':
         domain = 'legal.half.en'
         domain_out = 'out.mixed.legal.en'
@@ -129,8 +129,10 @@ def experiment_tfidfSVM(type_d, topics=100):
         uni_sentences = [sentence for sentence in doc.readlines()]
 
     in_sent = vectorizer.fit_transform(uni_sentences)
-    tsvd = TruncatedSVD(n_components=topics)
-    # tsvd = NMF(n_components=100)
+    if method is 'tsvd':
+        tsvd = TruncatedSVD(n_components=topics)
+    elif method is 'nmf':
+        tsvd = NMF(n_components=100, sparseness='data', init='nndsvd')
     t_in_sent = tsvd.fit_transform(in_sent)
     # mm_scale = MinMaxScaler()
     # t_in_sent = mm_scale.fit_transform(t_in_sent)
@@ -167,7 +169,7 @@ def experiment_tfidfSVM(type_d, topics=100):
     print 'Support:', s,
 
 
-def experiment_tfidfKLIEP(type_d, topics=100):
+def experiment_tfidfKLIEP(type_d, topics=100, method='tsvd'):
     if type_d is 'legal':
         domain = 'legal.half.en'
         domain_out = 'out.mixed.legal.en'
@@ -194,8 +196,10 @@ def experiment_tfidfKLIEP(type_d, topics=100):
         uni_sentences = [sentence for sentence in doc.readlines()]
 
     in_sent = vectorizer.fit_transform(uni_sentences)
-    tsvd = TruncatedSVD(n_components=topics)
-    # tsvd = NMF(n_components=100)
+    if method is 'tsvd':
+        tsvd = TruncatedSVD(n_components=topics)
+    elif method is 'nmf':
+        tsvd = NMF(n_components=100, sparseness='data', init='nndsvd')
     t_in_sent = tsvd.fit_transform(in_sent)
     # mm_scale = MinMaxScaler()
     # t_in_sent = mm_scale.fit_transform(t_in_sent)
@@ -207,9 +211,9 @@ def experiment_tfidfKLIEP(type_d, topics=100):
 
     labels = - np.ones(len(uni_sentences_out))
     labels[-50000:] = 1
-    out_sent = vectorizer.transform(uni_sentences_out)
-    # tsvd_out = TruncatedSVD(n_components=20)
-    t_out_sent = tsvd.transform(out_sent)
+    out_sent = vectorizer_out.fit_transform(uni_sentences_out)
+    tsvd_out = TruncatedSVD(n_components=topics)
+    t_out_sent = tsvd_out.fit_transform(out_sent)
     # t_out_sent = mm_scale.transform(t_out_sent)
     # print np.where(t_out_sent < 0)
     print 'parsed size:', t_out_sent.shape
@@ -218,7 +222,7 @@ def experiment_tfidfKLIEP(type_d, topics=100):
     kliep.fit_CV(t_out_sent, t_in_sent)
     predictions = - np.ones(len(uni_sentences_out))
     w = kliep.predict(t_out_sent).ravel()
-    predictions[np.where(w > 1.5)[0]] = 1   # w = p_te/p_tr
+    predictions[np.where(w > 1.1)[0]] = 1   # w = p_te/p_tr
     print 'total positive:', np.where(predictions == 1)[0].shape, ', out of:', t_out_sent.shape[0]
     # sorted_ind = np.argsort(w, axis=None)[::-1]
     # predictions[sorted_ind[0:50000]] = 1
@@ -292,7 +296,7 @@ def experiment_LMKLIEP(which_d):
     kliep.fit_CV(out_d, in_d)
 
     w = kliep.predict(out_d).ravel()
-    predictions[np.where(w > 1.5)[0]] = 1   # w = p_te/p_tr
+    predictions[np.where(w > 1)[0]] = 1   # w = p_te/p_tr
     print 'total positive:', np.where(predictions == 1)[0].shape, ', out of:', out_d.shape[0]
     # sorted_ind = np.argsort(w, axis=None)[::-1]
     # predictions[sorted_ind[0:50000]] = 1
@@ -325,7 +329,7 @@ def experimentLMKLIEP_per_feat(which_d):
         W[:, i] = kliep.predict(lm_out).ravel()
 
     w = np.mean(W, axis=1)
-    predictions[np.where(w > 1.5)[0]] = 1
+    predictions[np.where(w > 1.4)[0]] = 1
     print 'total positive:', np.where(predictions == 1)[0].shape, ', out of:', out_d.shape[0]
     # sorted_ind = np.argsort(w, axis=None)[::-1]
     # predictions[sorted_ind[0:50000]] = 1
@@ -339,21 +343,21 @@ def experimentLMKLIEP_per_feat(which_d):
 
 if __name__ == '__main__':
     # average pooling seems to work better than max pooling
-    # create_W2Vmodel('software', size_h=20)
+    create_W2Vmodel('software', size_h=20)
     # create_W2Vmodel('legal', size_h=20)
 
     # experiment_w2vSVM('legal', type_pooling='average_p')  # P:0.62, R: 0.49, F1: 0.55
-    # experiment_w2vSVM('software', type_pooling='average_p')
+    # experiment_w2vSVM('software', type_pooling='average_p')  # Precision: 0.354820175379 Recall: 0.51874 F1: 0.421400661256
 
-    # experiment_w2vKLIEP('legal', type_pooling='average_p')
-    # experiment_w2vKLIEP('software', type_pooling='average_p')
+    # experiment_w2vKLIEP('legal', type_pooling='average_p')  # Precision: 0.687512635622 Recall: 0.40808 F1: 0.512161449836
+    # experiment_w2vKLIEP('software', type_pooling='average_p')  # 0.543853551153 Recall: 0.37552 F1: 0.444276181913
 
-    # experiment_tfidfKLIEP('legal', topics=100)
-    # experiment_tfidfKLIEP('software', topics=100)
+    # experiment_tfidfKLIEP('legal', topics=5, method='tsvd')  # Precision: 0.144105604336 Recall: 0.77006 F1: 0.242778685062
+    # experiment_tfidfKLIEP('software', topics=5, method='tsvd')  # Precision: 0.0193548387097 Recall: 6e-05 F1: 0.000119629149636
 
-    # experiment_tfidfSVM('legal', topics=100)  # P: 0.32, R: 0.55, F1: 0.4
-    # experiment_tfidfSVM('software', topics=100)
+    # experiment_tfidfSVM('legal', topics=100, method='tsvd')  # P: 0.32, R: 0.55, F1: 0.4
+    # experiment_tfidfSVM('software', topics=100, method='tsvd')  # Precision: 0.22344679106 Recall: 0.52308 F1: 0.313131555002
 
-    # experimentLMKLIEP_per_feat('legal')
+    # experimentLMKLIEP_per_feat('legal')  # Precision: 0.222645099905 Recall: 0.27144 F1: 0.244633104418
     # construct_LMfeatVec('legal', parse=False)
-    experiment_LMKLIEP('legal')
+    # experiment_LMKLIEP('legal')  # Precision: 0.21700076066 Recall: 0.38798 F1: 0.278329363827
