2ae26df9d12c4a59560fd64b0a28b71f657f8135
saidalfaraby <said.al.faraby@gmail.com>
2014-06-03 15:29:17 +0200

    add spanish lm

--- Project3/FeatureExtraction.py
+++ Project3/FeatureExtraction.py
@@ -15,15 +15,23 @@ class Features(object):
     self.Term_Freq = defaultdict(int)  # term frequency. Key = term, Val = frequency
     self.POS_Freq = defaultdict(int)  # postag frequency. Key = POSTag, Val = Frequency
     self.TPOS_Freq = defaultdict(int)  # term, postag frequency. Key = (term,POSTag), Val = Frequency
-    self.Trans_Freq = defaultdict(int)  # translation frequency. Key = (E,F), Val = frequency
     self.N_Term = 0
 
+    self.Tr_Term_Freq = defaultdict(int) #for translation language model
+    self.Tr_TPOS_Freq = defaultdict(int) #for translation language model
+    self.Tr_POS_Freq = defaultdict(int)
+    self.Tr_N_Term = 0
+
+    
+    
     self.BTerm_Freq = defaultdict(int)  # bigram frequency. Key = (term, term), Val = Frequency
     self.BPOS_Freq = defaultdict(int)  # bigram POS-tags. key = (POSTag, POSTag), Val = Frequency
     self.BTPOS_Freq = defaultdict(int)
     self.BN_Term = 0
 
-  def parse_doc(self, path, nrandom_sample=None):
+    self.Trans_Freq = defaultdict(int)  # translation frequency. Key = (E,F), Val = frequency
+
+  def parse_doc(self, path, nrandom_sample=None, is_translation=False):
     regex = re.compile('[%s]' % re.escape(string.punctuation))
     i = 0
 
@@ -36,32 +44,35 @@ class Features(object):
       docs = newdocs
     #print len(docs)
     #print docs
-    for sentence in docs:
-      sentence = nltk.pos_tag(nltk.word_tokenize(sentence))
-      i += 1
-      print i
-      #remove punctuation
-      for token, pos in sentence:
-        try:
-          new_token = regex.sub(u'', token).decode('utf-8')
-          if not new_token == u'' and not new_token in stopwords.words('english'):
-            self.update_count(new_token, pos)
-        except:
-          pass
+    if not is_translation:
+      for sentence in docs:
+        sentence = nltk.pos_tag(nltk.word_tokenize(sentence))
+        i += 1
+        print i
+        #remove punctuation
+        for token, pos in sentence:
+          try:
+            new_token = regex.sub(u'', token).decode('utf-8')
+            if not new_token == u'' and not new_token in stopwords.words('english'):
+              self.update_count(new_token, pos)
+          except:
+            pass
 
-      # estimate statistics for bigrams
-      # words = [elem[0] for elem in sentence]
-      # pos_tags = [elem[1] for elem in sentence]
+        b_words, b_pos = self.to_bigram(sentence)
 
-      # b_words = nltk.bigrams(words)
-      # b_pos = nltk.bigrams(pos_tags)
-      b_words, b_pos = self.to_bigram(sentence)
-
-      for b_w, b_p in zip(b_words, b_pos):
-        self.update_count(b_w, b_p, bigrams=1)
-
+        for b_w, b_p in zip(b_words, b_pos):
+          self.update_count(b_w, b_p, bigrams=1)
+    else :
+      spanish_tagger = pickle.load(open('bitag_spanish.pickle','rb'))
+      for sentence in docs:
+        i += 1
+        print i
+        term_pos = spanish_tagger.tag(nltk.word_tokenize(sentence))
+        for term,pos in term_pos :
+          if not term == '' and not term in stopwords.words('spanish'):
+            self.update_count(term, pos, is_translation=True)
     self.set_lambda(1)
-    self.prune()
+    # self.prune()
 
   def to_bigram(self, termpos):
     words = [elem[0] for elem in termpos]
@@ -77,6 +88,9 @@ class Features(object):
     self.TPOS_Freq.default_factory = lambda: v
     self.Trans_Freq.default_factory = lambda: v
     self.POS_Freq.default_factory = lambda: v
+    self.Tr_Term_Freq.default_factory = lambda : v
+    self.Tr_TPOS_Freq.default_factory = lambda : v
+    self.Tr_POS_Freq.default_factory = lambda : v
     # bigram
     self.BTerm_Freq.default_factory = lambda: v
     self.BPOS_Freq.default_factory = lambda: v
@@ -89,10 +103,6 @@ class Features(object):
       if dictionary[k] <= 1:
         del dictionary[k]
         n_removed +=1
-        #if bigram == 0:
-        #  self.N_Term -= 1
-        #elif bigram == 1:
-        #  self.BN_Term -= 1
     return n_removed
 
   def prune(self):
@@ -107,17 +117,24 @@ class Features(object):
     bi_removed = self.prune_dict(self.BTPOS_Freq, bigram=1)
     self.BN_Term -= bi_removed
 
-  def update_count(self, t, p, bigrams=0):
-    if bigrams == 0:
-      self.Term_Freq[t] += 1
-      self.TPOS_Freq[(t, p)] += 1
-      self.N_Term += 1
-      self.POS_Freq[p] += 1
-    elif bigrams == 1:
-      self.BN_Term += 1
-      self.BTerm_Freq[t] += 1
-      self.BPOS_Freq[p] += 1
-      self.BTPOS_Freq[(t, p)] += 1
+  def update_count(self, t, p, bigrams=0, is_translation=False):
+    if not is_translation:
+      if bigrams == 0:
+        self.Term_Freq[t] += 1
+        self.TPOS_Freq[(t, p)] += 1
+        self.N_Term += 1
+        self.POS_Freq[p] += 1
+      elif bigrams == 1:
+        self.BN_Term += 1
+        self.BTerm_Freq[t] += 1
+        self.BPOS_Freq[p] += 1
+        self.BTPOS_Freq[(t, p)] += 1
+    else :
+      if bigrams == 0:
+        self.Tr_Term_Freq[t] += 1
+        self.Tr_TPOS_Freq[(t, p)] += 1
+        self.Tr_POS_Freq[p] += 1
+        self.Tr_N_Term += 1
 
   def update_count2(self, t, p, val, bigrams=0):
     if bigrams == 0:
@@ -212,6 +229,10 @@ class Features(object):
     storage['TPOS_Freq'] = self.TPOS_Freq
     storage['Trans_Freq'] = self.Trans_Freq
     storage['N_Term'] = self.N_Term
+    storage['Tr_Term_Freq'] = self.Tr_Term_Freq
+    storage['Tr_TPOS_Freq'] = self.Tr_TPOS_Freq
+    storage['Tr_POS_Freq'] = self.Tr_POS_Freq
+    storage['Tr_N_Term'] = self.Tr_N_Term
     # bigram
     storage['BPOS_Freq'] = self.BPOS_Freq
     storage['BTPOS_Freq'] = self.BTPOS_Freq
@@ -234,6 +255,18 @@ class Features(object):
     self.Trans_Freq = storage['Trans_Freq']
     self.N_Term = storage['N_Term']
 
+    try : 
+    # if True:
+      self.Tr_Term_Freq = storage['Tr_Term_Freq']
+      self.Tr_TPOS_Freq = storage['Tr_TPOS_Freq']
+      self.Tr_POS_Freq = storage['Tr_POS_Freq']
+      self.Tr_N_Term = storage['Tr_N_Term']
+    except :
+      self.Tr_Term_Freq = defaultdict(int)
+      self.Tr_TPOS_Freq = defaultdict(int)
+      self.Tr_POS_Freq = defaultdict(int)
+      self.Tr_N_Term = 0
+
     # bigram
     self.BPOS_Freq = storage['BPOS_Freq']
     self.BTPOS_Freq = storage['BTPOS_Freq']
@@ -251,20 +284,45 @@ class Features(object):
 
 if __name__ == '__main__':
     F = Features()
-    #F.parse_doc('project3_data_selection/legal.half.en')
-    F.parse_doc('project3_data_selection/out.mixed.legal.en')
-    #F.save('in_model.pickle')
+    # F.load('legal_in_model_noprune.pickle')
+    b1= len(F.Term_Freq)
+    # print F.Term_Freq.keys()
+    b2= len(F.POS_Freq)
+    b3= len(F.TPOS_Freq)
+
+    b4= F.N_Term
+    b5= len(F.BTerm_Freq)
+    b6= len(F.BPOS_Freq)
+    b7= len(F.BTPOS_Freq)
+    b8= F.BN_Term
+    F.parse_doc('project3_data_selection/legal.half.es',is_translation=True)
+    # F.parse_doc('project3_data_selection/out.mixed.legal.es')
+    F.save('legal_in_model_noprune_with_translation.pickle')
+    # F.load('legal_in_model_noprune_with_translation.pickle')
     #F.save('sample_mix_model.pickle')
-    F.save('mix_model.pickle')
-    #F.load('in_model.pickle')
+    # F.save('mix_model.pickle')
+    # F.load('legal_in_model_es.pickle')
+    print 'before'
+    print b1,b2,b3,b4,b5,b6,b7,b8
     print len(F.Term_Freq)
-    print F.Term_Freq.keys()
+    # print F.Term_Freq.keys()
     print len(F.POS_Freq)
     print len(F.TPOS_Freq)
+
     print F.N_Term
     print len(F.BTerm_Freq)
     print len(F.BPOS_Freq)
     print len(F.BTPOS_Freq)
     print F.BN_Term
+
+    print len(F.Tr_Term_Freq)
+    print len(F.Tr_TPOS_Freq)
+    print len(F.Tr_POS_Freq)
+    for i in range(100):
+      print F.Tr_TPOS_Freq.keys()[i]
+
+    print F.Tr_TPOS_Freq.keys().index(('dicha','aq0fsp'))
+
+    print F.Tr_N_Term
     # print F.Term_Freq.keys()[0]#, F.Term_Freq(F.Term_Freq.keys()[0])
     # print F.POS_Freq[('even','RB')]#, F.POS_Freq(F.POS_Freq.keys()[0])
